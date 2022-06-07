b7315fe293accf753ed616ffbf826ca74881f026
Ifeanyi <chimaifeanyi29@gmail.com>
2022-06-07 01:57:35 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="Header text-center">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                  <h1 className='h1'>Simple To-do Application</h1>
-                </div>
+      <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1 className='h1'>Simple To-do Application</h1>
             </div>
-        </div>
+          </div>
+      </div>
     </header>
   )
 }
